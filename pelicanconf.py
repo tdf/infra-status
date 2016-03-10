ce498277f51a649a6abdfa11a1de50ea23cce1af
Alexander Werner <alex@documentfoundation.org>
2016-03-10 19:01:36 +0100

    First release

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'The Document Foundation'
+SITENAME = 'The Document Foundation Infrastructure Status'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('The Document Foundation', 'https://documentfoundation.org/'),
+         ('LibreOffice', 'https://libreoffice.org/'),
+         ('Manitu Status', 'https://status.manitu.de/'),
+         ('Hetzner Status', 'https://www.hetzner-status.de/en.html'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+FILENAME_METADATA='(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
